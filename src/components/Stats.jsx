f8cfb91adbcde61752fa760465787cf9e0d205da
edward <edwardlfish@gmail.com>
2025-07-13 19:06:13 +0300

    Add ShmitterContext

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,11 +1,15 @@
 import Avatar from "./Avatar.jsx";
+import {useContext} from "react";
+import {ShmitterContext} from "../utils/context.js";
 
-const Stats = ({user, stats, changeAvatar}) => {
+const Stats = () => {
+    const {user, stats} = useContext(ShmitterContext);
+
     return (
         <div className={'user-stats'}>
             <div>
-                <Avatar changeAvatar={changeAvatar} user={user}/>
-                { user.name}
+                <Avatar/>
+                {user.name}
             </div>
             <div className={'stats'}>
                 <div>Followers: {stats.followers}</div>
