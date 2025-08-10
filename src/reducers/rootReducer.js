843de8a7e4088a55761496aa896b54ae4af39f36
ivanOleinick <oleinick.eminemkod31317@gmail.com>
2025-08-10 18:59:43 +0300

    first commit

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,21 @@
+import {userReducer} from "./userReducer.js";
+import {statsReducer} from "./statsReducer.js";
+
+// typeof State = {
+//     user: {
+//         name: string,
+//         avatar: string
+//     },
+//     stats: {
+//         followers: number,
+//         following: number
+//     }
+// }
+
+
+export const rootReducer = (state, action) => {
+   return{
+       user: userReducer(state.user, action),
+       stats: statsReducer(state.stats, action)
+   }
+}
