f7f00a09e1589560ab362546f0c67119816fb849
ivanOleinick <oleinick.eminemkod31317@gmail.com>
2025-08-10 19:18:30 +0300

    combineReducers

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,12 @@
 import {CHANGE_AVATAR, CHANGE_NAME} from "../actions/userAction.js";
 
+const defaultState = {
+    name: 'Monster',
+    avatar: 'https://gravatar.com/avatar/000?d=monsterid'
+};
 
-export const userReducer = (state, action) => {
+
+export const userReducer = (state=defaultState, action) => {
     switch (action.type) {
         case CHANGE_AVATAR: {
             return {...state, avatar: action.payload || state.avatar};
